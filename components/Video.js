c8d608d34b4cdf417bd7f0006b0ba17112536cd9
prashanth acharya <prashanth.ocean@gmail.com>
2019-01-07 00:29:22 +0530

    use orientation locker

--- components/Video.js
+++ components/Video.js
@@ -12,7 +12,7 @@ import {
 } from 'react-native'
 import VideoPlayer from 'react-native-video'
 import KeepAwake from 'react-native-keep-awake'
-import Orientation from 'react-native-orientation'
+import Orientation from 'react-native-orientation-locker'
 import Icons from 'react-native-vector-icons/MaterialIcons'
 import { Controls } from './'
 import { checkSource } from './utils'
